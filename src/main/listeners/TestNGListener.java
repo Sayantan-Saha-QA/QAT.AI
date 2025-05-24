6c1947b1de2aaca31e4b417ef320beb1b16dda3e
S-Saha-GenAI-Consulting <sayantan.saha@outlook.in>
2025-05-24 19:19:52 +0530

    commit

--- src/main/listeners/TestNGListener.java
+++ src/main/listeners/TestNGListener.java
@@ -10,11 +10,16 @@ public class TestNGListener implements ITestListener {
     public void onTestFailure(ITestResult result) {
         System.out.println("Test failed: " + result.getName());
     }
-
-    @Override public void onTestStart(ITestResult result) {}
-    @Override public void onTestSuccess(ITestResult result) {}
-    @Override public void onTestSkipped(ITestResult result) {}
-    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
-    @Override public void onStart(ITestContext context) {}
-    @Override public void onFinish(ITestContext context) {}
+    @Override public void onTestStart(ITestResult result) {
+        System.out.println("Test passed: " + result.getName());
+    }
+    @Override public void onTestSkipped(ITestResult result) {
+        System.out.println("Test skipped: " + result.getName());
+    }
+    @Override public void onStart(ITestContext context) {
+        System.out.println("Test started: " + context.getName());
+    }
+    @Override public void onFinish(ITestContext context) {
+        System.out.println("Test finished: " + context.getName());
+    }
 }
